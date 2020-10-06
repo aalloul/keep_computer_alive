6c2f9f7194c13b262e8729c9ab853ea0241e9c73
aa01163 <adam.alloul@randstadgroep.nl>
2020-10-06 17:21:58 +0200

    added duration

--- keep_alive.py
+++ keep_alive.py
@@ -1,8 +1,11 @@
 from pynput.keyboard import Controller, Key
-from time import sleep
+from time import sleep, time
 
 keyboard = Controller()
-while True:
+duration = 3*3600
+start_date = time()
+
+while start_date + duration > time():
     keyboard.press(Key.f15)
     keyboard.release(Key.f15)
     sleep(20)

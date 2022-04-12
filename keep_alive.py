35ddf252956b8482aa230a6fc1b9b49c669968e5
B03572 <adam.alloul@nl.abnamro.com>
2022-04-12 08:11:40 +0200

    Add notification when timer is over

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,22 @@
-from pynput.keyboard import Controller, Key
+# from pynput.keyboard import Controller, Key
+from pynput.mouse import Controller as MouseController
 from time import sleep, time
+from plyer import notification
 
-keyboard = Controller()
-duration = 3*3600
+# keyboard = Controller()
+mouse = MouseController()
+duration = 8.5*3600
 start_date = time()
 
 while start_date + duration > time():
-    keyboard.press(Key.f15)
-    keyboard.release(Key.f15)
+    mouse.move(1, 0)
+    # keyboard.press(Key.f15)
+    # keyboard.release(Key.f15)
     sleep(20)
+
+notification.notify(
+    title='Time',
+    message='Keep alive process finished',
+    app_icon=None,
+    timeout=10,
+)
